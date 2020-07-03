7542a537ca13e87f7bc3f0beefe5295c1b2b3623
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-07-02 18:11:35 -0700

    Updates

---
+++ precache-manifest.b8e28b2b8fe1da295e1fda9857ba1394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e761e74d1eface0ba089fe0b8f5d1d",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "fb377aa24dbfff5c4d7c",
+    "url": "/react-router/static/css/main.3f37ee45.chunk.css"
+  },
+  {
+    "revision": "9dbfc9e2d574561f9a5c",
+    "url": "/react-router/static/js/2.3a959338.chunk.js"
+  },
+  {
+    "revision": "5b2dc2e96f9c06b2445e2f7662108a31",
+    "url": "/react-router/static/js/2.3a959338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb377aa24dbfff5c4d7c",
+    "url": "/react-router/static/js/main.0f4cabd7.chunk.js"
+  },
+  {
+    "revision": "b8b9503883235406b043",
+    "url": "/react-router/static/js/runtime-main.675fa452.js"
+  }
+]);
